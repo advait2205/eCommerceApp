869cc070d7b2bb0715780f633372e5a68931cac7
advait acharya <advaitacharya22@gmail.com>
2021-03-21 21:08:19 +0530

    Added View Activity For Adding Product

--- app/src/main/java/com/advait/ecommerceapp/Admin/AdminAddNewProductActivity.java
+++ app/src/main/java/com/advait/ecommerceapp/Admin/AdminAddNewProductActivity.java
@@ -3,6 +3,10 @@ package com.advait.ecommerceapp.Admin;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.advait.ecommerceapp.R;
@@ -10,12 +14,19 @@ import com.advait.ecommerceapp.R;
 public class AdminAddNewProductActivity extends AppCompatActivity {
 
     private String categoryName;
+    private Button addNewProductBtn;
+    private EditText inputProductName, inputProductDescription, inputProductPrice;
+    private ImageView inputProductImg;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_add_new_product);
         categoryName = getIntent().getExtras().get("category").toString();
-        Toast.makeText(this, categoryName, Toast.LENGTH_SHORT).show();
+        addNewProductBtn = (Button) findViewById(R.id.add_new_product);
+        inputProductName = (EditText) findViewById(R.id.product_name);
+        inputProductDescription = (EditText) findViewById(R.id.product_description);
+        inputProductPrice = (EditText) findViewById(R.id.product_price);
+        inputProductImg = (ImageView) findViewById(R.id.select_product_img);
     }
 }
